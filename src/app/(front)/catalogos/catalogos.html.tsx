40cca59975e05f70e40481e248136aba8707f47e
cesar <rivera.c.it@opermx.net>
2024-06-12 00:10:38 -0600

    catalogos y lista de precios

--- src/app/(front)/catalogos/catalogos.html.tsx
+++ src/app/(front)/catalogos/catalogos.html.tsx
@@ -1,14 +1,105 @@
 const CatalogosTemplate = () => (
 
 <main>
-    <div className="container py-4 px-3 mx-auto">
-      <h1>Catálogos</h1>
-      <div className="col-lg-8 px-0">
-        <p className="fs-4">
-          Mosaico de Catalogos
-        </p>
+    <div className="container-fluid hero-sections mx-auto">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>CATÁLOGOS</h1>
+          </div>
+        </div>
       </div>
-      
+    </div>
+    <div className="container products-cards">
+      <div className="row">
+        <h2>Marcas</h2>
+      </div>
+      <div className="row row-cols-2 row-cols-md-4 g-4">
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-3.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-4.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-5.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-3.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-4.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-5.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
--- src/app/(front)/precios/precios.html.tsx
+++ src/app/(front)/precios/precios.html.tsx
@@ -1,14 +1,105 @@
 const PreciosTemplate = () => (
 
 <main>
-    <div className="container py-4 px-3 mx-auto">
-      <h1>Precios</h1>
-      <div className="col-lg-8 px-0">
-        <p className="fs-4">
-          Mosaico de precios
-        </p>
+    <div className="container-fluid hero-sections mx-auto">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>LISTA DE PRECIOS</h1>
+          </div>
+        </div>
       </div>
-      
+    </div>
+    <div className="container products-cards">
+      <div className="row">
+        <h2>Marcas</h2>
+      </div>
+      <div className="row row-cols-2 row-cols-md-4 g-4">
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-3.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-4.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-5.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-3.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-4.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-5.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-1.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+        <div className="col text-center">
+          <div className="card mx-auto d-block">
+          <img src="/../images/logos/logo-demo-2.webp" className="card-img-top"
+              alt="Labor Wasser México" />
+            <a href="#">CONSULTAR</a>
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 
