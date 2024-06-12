43dd2c6c68cdc88873fd2ad061e1497e6d392597
Gabino Ramírez <jadwer@msn.com>
2024-06-11 23:50:43 -0600

    Catalogs and Prices Static pages Templates

---
+++ src/app/(front)/precios/page.tsx
@@ -0,0 +1,9 @@
+import PreciosTemplate from "./precios.html"
+
+const PreciosPage = () => {
+  return (
+    <PreciosTemplate />
+  )
+}
+
+export default PreciosPage
---
+++ src/app/(front)/precios/precios.html.tsx
@@ -0,0 +1,16 @@
+const PreciosTemplate = () => (
+
+<main>
+    <div className="container py-4 px-3 mx-auto">
+      <h1>Precios</h1>
+      <div className="col-lg-8 px-0">
+        <p className="fs-4">
+          Mosaico de precios
+        </p>
+      </div>
+      
+    </div>
+  </main>
+
+
+); export default PreciosTemplate;
