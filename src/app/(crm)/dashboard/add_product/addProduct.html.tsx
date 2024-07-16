25bc261610fa69b0b892596287a48b8b4f77c304
Gabino Ramírez <jadwer@msn.com>
2024-07-15 22:18:38 -0600

    add new product component

---
+++ src/app/(crm)/dashboard/add_product/addProduct.html.tsx
@@ -0,0 +1,21 @@
+const AddProductTemplate = () => (
+
+    <main>
+        <div className="container-fluid back-header">
+            <div className="row my-4 align-items-md-center">
+                <div className="col-12 col-md-6">
+                    <h4>Agregar nuevo producto</h4>
+                </div>
+            </div>
+        </div>
+        <div className="container-fluid container-product">
+
+        Aqui mi formulario
+                <div className="col-12 col-md-6 d-flex justify-content-end">
+                    <button type="button" className="btn btn-primary ms-2">Agregar producto</button>
+                </div>
+
+        </div>
+    </main>
+
+); export default AddProductTemplate;
