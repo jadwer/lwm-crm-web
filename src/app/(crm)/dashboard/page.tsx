4d6e8da767e3c63a6f6cdde1e4cd30168e0dead8
Gabino Ramírez <jadwer@msn.com>
2024-07-20 21:31:32 -0600

    Solved dashboard build error

--- src/app/(crm)/dashboard/page.tsx
+++ src/app/(crm)/dashboard/page.tsx
@@ -1,9 +1,31 @@
+'use client'
+
+import { Suspense, useEffect, useState } from "react"
 import ProductsTemplate from "./products/products.html"
+import { useProducts } from "@/hooks/products";
 
 const DashboardPage = () => {
-  return (
-    <ProductsTemplate />
-  )
+  const [productos, setProductos] = useState([]);
+  const {getAllProducts} = useProducts();
+
+  useEffect(() => {
+    getAllProducts({setProductos});
+  }, []);
+
+  if(Object.keys(productos).length === 0){
+    return (
+      <Suspense>
+        <h2>Loading...</h2>
+      </Suspense>
+    )
+  } else {
+    console.log(typeof(productos))
+    return (
+      <Suspense>
+        <ProductsTemplate data={{productos}}/>
+      </Suspense>
+    )
+  }
 }
 
 export default DashboardPage
