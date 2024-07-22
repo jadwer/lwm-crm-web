cd1998592217500cc21d4812eb1dafe2ca5f2fa5
Gabino Ramírez <jadwer@msn.com>
2024-07-21 18:06:19 -0600

    Contact form success page

---
+++ src/app/(custom)/contact-success/csuccess.html.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ContactSuccessTemplate = () => { return (
+
+<main>
+  El mensaje fue enviado con éxito
+</main>
+
+
+)}; export default ContactSuccessTemplate;
---
+++ src/app/(custom)/contact-success/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ContactSuccessTemplate from "./csuccess.html"
+
+const ContactSuccessPage = () => {
+  return (
+    <ContactSuccessTemplate />
+  )
+}
+
+export default ContactSuccessPage
